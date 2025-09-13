55e70ae7f0d883431857db70de3cc9b4681fc980
Anugrah <amvanugrah@gmail.com>
2025-09-13 19:40:54 +0530

    ath views change

--- questions/services.py
+++ questions/services.py
@@ -6,7 +6,7 @@ GEMINI_API_KEY = "AIzaSyBsfin-yjWUsYIYoKU8NijUa4v0l-mgXZQ" # Set your key in you
 def generate_interview_questions(field, difficulty, num_questions):
     prompt = (
         f"Generate {num_questions} unique interview questions for a {field} interview. "
-        f"Difficulty: {difficulty}. Return only plain questions, no answers, as a numbered list."
+        f"Difficulty: {difficulty}. Return only plain questions, no answers, as a numbered list. this questions are going to be read by a voice assistant so do not use  any special charactors which might break the voice assistant. "
     )
     payload = {
         "contents": [
@@ -20,7 +20,8 @@ def generate_interview_questions(field, difficulty, num_questions):
     response = requests.post(GEMINI_URL, json=payload, headers=headers)
     response.raise_for_status()
     content = response.json()
-    print(content)  # For debugging
+    # For debugging
+    print(content)
 
     # Extract the text string properly
     text = content['candidates'][0]['content']['parts'][0]['text']
