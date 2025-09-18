5fd4498aa0397541a7f0519dd291524a76263644
Anugrah <amvanugrah@gmail.com>
2025-09-18 20:12:41 +0530

    login api url changed

--- ai_interview_coach/urls.py
+++ ai_interview_coach/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/interview/', include('interviews.urls')),
     path('api/questions/', include('questions.urls')),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
